7ce3c641d3bb126e24cb0073a28f215809560544
walaazahran <walaazahran40@gmail.com>
2023-07-08 19:30:11 +0300

    style property of the component

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: './notification.component.html',
+  styles: ["div{margin:10px 0px;padding:10px 20px;background-color:#fad719;}", "p{color:red;}"]
+})
+export class NotificationComponent {
+
+}
