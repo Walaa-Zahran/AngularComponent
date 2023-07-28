2fd5ea240d5f2e30a4aaea65bc8ed95a037cce1b
walaazahran <walaazahran40@gmail.com>
2023-07-28 13:28:17 +0300

    Different selectors in angular and using bootstrap

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
-  styles: ["div{margin:10px 0px;padding:10px 20px;background-color:#fad719;}", "p{color:red;}"]
+  styleUrls: ['./notification.component.css'],
 })
 export class NotificationComponent {
 
