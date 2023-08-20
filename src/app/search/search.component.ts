0fbceaf2b06288d9f4cbd8c491459defdbe1c008
walaazahran <walaazahran40@gmail.com>
2023-08-20 18:26:19 +0300

    Two way data binding | Data Binding

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
-  searchValue: string = '';
+  searchValue: string = 'Iphone';
   changeSearchValue(eventData: any) {
     // console.log(eventData.target.value);
     this.searchValue = eventData.target.value;
